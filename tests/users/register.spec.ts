1eff87687527ef86d04ed36aceb41f35d6720529
Git_Koushik <programmer747767@gmail.com>
2025-02-15 19:37:22 +0530

    test: enhance user registration tests with additional assertions for JSON response and user properties

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -37,6 +37,7 @@ describe("POST /auth/register", () => {
       // Assert
       expect(response.statusCode).toBe(201);
     });
+
     it("should return json data", async () => {
       // AAA
       // Arrange
@@ -53,6 +54,7 @@ describe("POST /auth/register", () => {
         (response.headers as Record<string, string>)["content-type"]
       ).toEqual(expect.stringContaining("json"));
     });
+
     it("should should persists the users in database", async () => {
       // Arrange
       const userData = {
@@ -72,6 +74,7 @@ describe("POST /auth/register", () => {
       expect(users[0].gmail).toBe(userData.gmail);
       expect(users[0].firstName).toBe("Koushik");
     });
+
     it("should persist the user in the database and return an id", async () => {
       const userData = {
         firstName: "Koushik",
@@ -91,6 +94,7 @@ describe("POST /auth/register", () => {
       expect(user).toBeDefined();
       expect(user?.id).toBe(responseBody.id);
     });
+
     it("should describe the role of users", async () => {
       const userData = {
         firstName: "Koushik",
@@ -106,6 +110,7 @@ describe("POST /auth/register", () => {
       expect(users[0]).toHaveProperty("role");
       expect(users[0].role).toBe(roles.CUSTOMER);
     });
+
     it("should describe the hash of password", async () => {
       const userData = {
         firstName: "Koushik",
@@ -122,6 +127,7 @@ describe("POST /auth/register", () => {
       expect(users[0].password).toHaveLength(60);
       expect(users[0].password).toMatch(/^\$2[aby]\$\d{2}\$[./A-Za-z0-9]{53}$/);
     });
+
     it("should describe email is already exists", async () => {
       // Arrange
       const userData = {
