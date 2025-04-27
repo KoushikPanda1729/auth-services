b3f1a3f1878f3db688c2bb2fd939c1ce4a075181
Git_Koushik <programmer747767@gmail.com>
2025-04-27 12:47:46 +0530

    refactor: simplify logger configuration by removing redundant format definitions

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -6,33 +6,26 @@ export const logger = winston.createLogger({
   defaultMeta: {
     serviceName: "auth-services",
   },
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
   transports: [
     new winston.transports.File({
       level: "info",
       dirname: "log",
       filename: "app.log",
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json()
-      ),
+
       silent: Config.NODE_ENV === "test",
     }),
     new winston.transports.File({
       level: "error",
       dirname: "log",
       filename: "error.log",
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json()
-      ),
       silent: Config.NODE_ENV === "test",
     }),
     new winston.transports.Console({
       level: "info",
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json()
-      ),
       silent: Config.NODE_ENV === "test",
     }),
   ],
