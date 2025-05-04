5119f48ced1e7f9fe5bf9e216f250e9fc131e9ac
Git_Koushik <programmer747767@gmail.com>
2025-05-04 14:32:37 +0530

    add migration

--- src/migration/1746348597434-migration.ts
+++ src/migration/1746348597434-migration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Migration1746344892610 implements MigrationInterface {
-  name = "Migration1746344892610";
+export class Migration1746348597434 implements MigrationInterface {
+  name = "Migration1746348597434";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
