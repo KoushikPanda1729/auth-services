e770b5273ca07dda74b31a2d4d4733c559f2d03c
Git_Koushik <programmer747767@gmail.com>
2025-06-02 15:19:29 +0530

    replace bcrypt to bcryptjs

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import createHttpError from "http-errors";
 import { Brackets, Repository } from "typeorm";
 import { User } from "../entity/User";
